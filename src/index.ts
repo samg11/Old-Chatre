6a61de8b36ea7b0319e7d1f1b9161fbc78328af2
Sam Girshovich <sam.girshovich@gmail.com>
2021-03-16 09:46:31 -0400

    ejs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import path from "path";
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render('index');
 });
 
 app.listen(PORT, () => {
