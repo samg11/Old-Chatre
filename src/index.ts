8fbf8949d7e67b84c4179d3729bcb5408d2068cf
Sam Girshovich <sam.girshovich@gmail.com>
2021-03-15 23:18:58 -0400

    hello world

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running in http://localhost:${PORT}`);
+});
