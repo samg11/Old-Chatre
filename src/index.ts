b87bcc26038c66dd44070ef090e4f28e72916994
Sam Girshovich <sam.girshovich@gmail.com>
2021-03-16 13:31:41 -0400

    partials, jquery, bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express from "express";
+import path from "path";
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use('/bootstrap', express.static(path.join('node_modules', 'bootstrap', 'dist')))
+app.use('/jquery', express.static(path.join('node_modules', 'jquery', 'dist')))
+
 
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-  res.render('index');
+  res.render('pages/index');
 });
 
 app.listen(PORT, () => {
