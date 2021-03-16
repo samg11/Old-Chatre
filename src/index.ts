f5089ac1636739a0e6990210ea6a5ff50c62bf76
Sam Girshovich <sam.girshovich@gmail.com>
2021-03-16 13:13:11 -0400

    listening

--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running in http://localhost:${PORT}`);
+  console.log(`Server is listening on http://localhost:${PORT}`);
 });
