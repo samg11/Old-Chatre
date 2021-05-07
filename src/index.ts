2d3bfa111c5702a49e3b6914e5b0da5973fd8048
Sam Girshovich <sam.girshovich@gmail.com>
2021-05-07 11:06:41 -0400

    test

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,6 @@
-import express from "express";
-import morgan from "morgan";
-
-import groups from "./groups";
-import chat from "./chat";
+import { app } from './app';
 
 const PORT = process.env.PORT || 8080;
-
-const app = express();
-
-// COMPRESSION
-app.use(require('compression')())
-
-// BODY PARSER
-app.use(express.json())
-
-// LOGGER
-app.use(morgan(`:method :status :response-time ms :url`));
-
-// static routes
-app.use('/public', express.static('public'));
-
-// EXPRESS MIDDLEWARE
-app.use('/groups', groups);
-app.use('/chat', chat);
-
-// set ejs as template engine
-app.set('view engine', 'ejs');
-
-// index route
-app.get("/", (req, res) => {
-  res.render('pages/index');
-});
 
 // runs web server
 app.listen(PORT, () => {
---
+++ tests/app.test.ts
@@ -0,0 +1,11 @@
+const {app} = require('../dist/app');
+
+const supertest = require('supertest')
+const request = supertest(app)
+
+it('Test API', async done => {
+    const response = await request.get('/test');
+  
+    expect(response.status).toBe(200);
+    done();
+  })
