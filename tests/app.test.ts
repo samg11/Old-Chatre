665cfd325b2a5e292c020dd32e75073ffa47a719
Sam Girshovich <sam.girshovich@gmail.com>
2021-05-08 19:51:02 -0400

    test indents fixed

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -4,8 +4,7 @@ const supertest = require('supertest')
 const request = supertest(app)
 
 it('Test API', async done => {
-    const response = await request.get('/test');
-  
-    expect(response.status).toBe(200);
-    done();
-  })
+  const response = await request.get('/test');
+  expect(response.status).toBe(200);
+  done();
+})
